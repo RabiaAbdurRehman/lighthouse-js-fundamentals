db178ccd35252f95763275e4a70f9d2e3e491b90
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-05-02 01:46:43 -0600

    how many hundreds

---
+++ howmanyhundreds.js
@@ -0,0 +1,16 @@
+const howManyHundreds = function (number){
+
+    let howmany = Math.floor(number/100);
+    return howmany;
+}
+
+
+
+
+
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
