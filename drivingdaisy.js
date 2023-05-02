4c14c94b83a12669e215c96748e66fd39596066e
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-05-02 16:27:30 -0600

    driving daisy

---
+++ drivingdaisy.js
@@ -0,0 +1,29 @@
+function carPassing(cars, speed){
+    const newcarPassing = { speed: speed,
+                   time: Date.now()
+                  };
+    cars.push(newcarPassing);
+    return cars;
+  }
+  const cars = [
+    {
+      time: 1568329654807,
+      speed: 40
+    },
+    {
+      time: 1568329821632,
+      speed: 42
+    },
+    {
+      time: 1568331115463,
+      speed: 35
+    }
+  ]
+    
+  const speed = 38
+    
+  console.log(carPassing(cars, speed));
+  
+    
+  console.log(carPassing(cars, speed));
+  
