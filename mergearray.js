c751d3fcc76a4e8951c87e03381bff8a4047a32b
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-05-04 15:31:05 -0600

    merging and sorting two arrays stretch

---
+++ mergearray.js
@@ -0,0 +1,18 @@
+function merge(arr1, arr2){
+    
+    for(i = 0; i < arr1.length; i++){
+        arr2.push(arr1[i]);
+    }
+    arr2.sort();;
+    return arr2;
+    }
+
+
+
+
+
+
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
